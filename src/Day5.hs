0e439505ce4eb507d4600a758be2f83bd8b9dac9
Yc.S <onixie@gmail.com>
2019-12-11 07:47:20 +0900

    Day5 Part1

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Day5 where
 
@@ -12,4 +13,39 @@ code = read <$> (option "" (string "-") <++> munch isDigit)
 program :: ReadP [Int]
 program = (code `sepBy` char ',') <* optional (char '\n') <* eof
 
-readInput = readFile "data/Day5-input.txt" >>= return . last . readP_to_S program
+readInput = do
+  (i, "") <- readFile "data/Day5-input.txt" >>= return . last . readP_to_S program
+  return i
+
+interpret' :: Int -> [Int] -> IO [Int]
+interpret' ip icp = case opc'm icp ip of
+  (99, _) -> return icp
+  (1, ms) -> eval (+) icp ip ms >>= interpret' (ip+4)
+  (2, ms) -> eval (*) icp ip ms >>= interpret' (ip+4)
+  (3, _)  -> save icp ip        >>= interpret' (ip+2)
+  (4, _)  -> load icp ip        >>  interpret' (ip+2) icp
+  (opc,_) -> undefined
+  where
+    eval op icp ip ms = do
+        print $ get icp (ip+1) (ms!!0) `op` get icp (ip+2) (ms!!1)
+        return . update icp (ip+3) $ get icp (ip+1) (ms!!0) `op` get icp (ip+2) (ms!!1)
+    save icp ip = return $ update icp (ip+1) 1 --readLn >>= return . update icp pi
+    load icp ip = print (icp !! (ip+1))
+    update icp ip nv =
+      let (h, x:t) = splitAt (icp !! ip) icp in
+        h ++ (nv:t)
+    deref icp ip = icp !! (icp !! ip)
+    get icp ip 0 = deref icp ip
+    get icp ip 1 = icp !! ip
+
+opc'm :: [Int] -> Int -> (Int, [Int])
+opc'm pp ip = let (ms, op) = (pp !! ip) `quotRem` 100 in
+                (op, pad $ md ms)
+  where
+    md 0  = []
+    md ms = let (ms', m) = ms `quotRem` 10 in m:md ms'
+    pad ms = take 2 (ms++repeat 0)
+
+interpret pp = interpret' 0 pp
+
+day5Part1 = readInput >>= interpret
