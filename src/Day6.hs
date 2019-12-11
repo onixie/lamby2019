74087e5fba962892516504dd85252fee745f0bfe
Yc.S <onixie@gmail.com>
2019-12-11 22:51:29 +0900

    Day2 Part2

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TupleSections #-}
 module Day6 where
 
@@ -7,16 +8,42 @@ import Control.Monad.IO.Class
 
 orbit  = char ')'
 object = munch (\x -> x/=')' && x/='\n')
+uomap = ((,) <$> (object <* orbit) <*> object) `sepBy` char '\n'
 
-uomap = ((,) <$> (object <* orbit) <*> object) `sepBy` lf
-  where lf = char '\n'
+inner = fst
+outer = snd
 
-readMap = last . readP_to_S uomap <$> readFile "data/Day6-sample.txt"
+readMap = fst . last . readP_to_S uomap <$> readFile "data/Day6-input.txt"
 
-orbitedBy (obj, lv) = return . ((obj, lv), ) . map ((,lv+1) . snd) . filter ((obj==) . fst)
+type UOTreeBuilder = forall m. MonadIO m => [(String, String)] -> m (Tree (String , Integer))
 
-buildFrom root uomap = unfoldTreeM (flip orbitedBy uomap) (root,0)
+buildFrom root dir uomap = unfoldTreeM (flip dir uomap) (root,0)
 
-visual = readMap >>= buildFrom "COM" . fst >>= putStrLn . drawTree . fmap show
+obj1 `to` obj2 = \(obj, lv) -> return . ((obj, lv), ) . map ((,lv+1) . obj2) . filter ((obj==) . obj1)
 
-day6Part1 = readMap >>= buildFrom "COM" . fst >>= return . sum . fmap snd . flatten
+fromCOM :: UOTreeBuilder
+fromCOM = buildFrom "COM" (inner `to` outer)
+
+fromSAN :: UOTreeBuilder
+fromSAN = buildFrom "SAN" (outer `to` inner)
+
+fromYOU :: UOTreeBuilder
+fromYOU = buildFrom "YOU" (outer `to` inner)
+
+visualize = putStrLn . drawTree . fmap show
+
+drawFromCOM = readMap >>= fromCOM >>= visualize
+
+day6Part1 = readMap >>= fromCOM >>= return . sum . fmap snd . flatten
+
+day6Part2 = do
+    uomap <- readMap
+    treeFromSAN <- fromSAN uomap >>= return . flatten
+    treeFromYOU <- fromYOU uomap >>= return . flatten
+    --print (reverse treeFromSAN)
+    --print (reverse treeFromYOU)
+    return $ findJoin (reverse treeFromSAN) (reverse treeFromYOU)
+  where
+    findJoin ((x,n):xs) ((y,m):ys)
+      | x == y = findJoin xs ys
+      | otherwise = n+m
