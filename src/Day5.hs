6e5b51a6ece3c43bd43b3bd0a2fdbdc51008253a
Yc.S <onixie@gmail.com>
2019-12-11 11:00:30 +0900

    Day5 Refactor Part1

--- src/Day5.hs
+++ src/Day5.hs
@@ -6,7 +6,7 @@ import Data.Char
 import Text.ParserCombinators.ReadP
 import Text.Printf
 
-pa <++> pb = ((++) <$> pa <*> pb)
+pa <++> pb = (++) <$> pa <*> pb
 
 code :: ReadP Int
 code = read <$> (option "" (string "-") <++> munch isDigit)
@@ -15,24 +15,22 @@ program :: ReadP [Int]
 program = (code `sepBy` char ',') <* optional (char '\n') <* eof
 
 readInput = do
-  (i, "") <- readFile "data/Day5-input.txt" >>= return . last . readP_to_S program
+  (i, "") <- last . readP_to_S program <$> readFile "data/Day5-input.txt"
   return i
 
-interpret' :: Int -> [Int] -> IO [Int]
-interpret' ip icp = case opc'm icp ip of
+interpretFrom :: Int -> [Int] -> IO [Int]
+interpretFrom ip icp = case opc'm icp ip of
   (99, _) -> return icp
-  (1, ms) -> printf "%d ADD (MODE=%s)\n" ip (show ms) >> eval (+) icp ip ms >>= interpret' (ip+4)
-  (2, ms) -> printf "%d MUL (MODE=%s)\n" ip (show ms) >> eval (*) icp ip ms >>= interpret' (ip+4)
-  (3, _)  -> printf "%d IN\n"  ip                     >> readIn   icp ip    >>= interpret' (ip+2)
-  (4, _)  -> printf "%d OUT="  ip                     >> printOut icp ip    >>  interpret' (ip+2) icp
+  (1, ms) -> printf "%d ADD (MODE=%s) %d %d %d\n" ip (show ms) (icp!!(ip+1)) (icp!!(ip+2)) (icp!!(ip+3)) >> eval (+) icp ip ms >>= interpretFrom (ip+4)
+  (2, ms) -> printf "%d MUL (MODE=%s) %d %d %d\n" ip (show ms) (icp!!(ip+1)) (icp!!(ip+2)) (icp!!(ip+3)) >> eval (*) icp ip ms >>= interpretFrom (ip+4)
+  (3, _)  -> printf "%d IN %d\n"  ip (icp!!(ip+1))                                                       >> readIn   icp ip    >>= interpretFrom (ip+2)
+  (4, _)  -> printf "%d OUT %d="  ip (icp!!(ip+1))                                                       >> printOut icp ip    >>  interpretFrom (ip+2) icp
   (opc,_) -> printf "%d ERROR" >> undefined
   where
-    eval op icp ip ms = do
---        print $ get icp (ip+1) (ms!!0) `op` get icp (ip+2) (ms!!1)
-        return . update icp (ip+3) $ get icp (ip+1) (ms!!0) `op` get icp (ip+2) (ms!!1)
-    readIn   icp ip = return $ update icp (ip+1) 1 --readLn >>= return . update icp pi
-    printOut icp ip = print $ deref icp (ip+1)
-    update icp ip nv =
+    eval op icp ip ms = return . update icp (ip+3) $ get icp (ip+1) (ms!!0) `op` get icp (ip+2) (ms!!1)
+    readIn   icp ip   = return $ update icp (ip+1) 1 --readLn >>= return . update icp pi
+    printOut icp ip   = print $ deref icp (ip+1)
+    update icp ip nv  =
       let (h, x:t) = splitAt (icp !! ip) icp in
         h ++ (nv:t)
     deref icp ip = icp !! (icp !! ip)
@@ -47,6 +45,6 @@ opc'm pp ip = let (ms, op) = (pp !! ip) `quotRem` 100 in
     md ms = let (ms', m) = ms `quotRem` 10 in m:md ms'
     pad ms = take 2 (ms++repeat 0)
 
-interpret pp = interpret' 0 pp
+interpret = interpretFrom 0
 
-day5Part1 = readInput >>= interpret
+day5Part1 = readInput >>= interpret >> return ()
