dba0a7af5bbe6eef644ab0e51cf312b9234828ae
Yc.S <onixie@gmail.com>
2019-12-18 18:08:00 +0900

    Day13 Part2

--- src/Day13.hs
+++ src/Day13.hs
@@ -15,34 +15,39 @@ import Diagrams.Backend.SVG
 data Tile = Empty | Wall | Block | Paddle | Ball deriving (Show, Eq)
 type Position = (Double, Double)
 type Game = Map Position Tile
+type Score = Integer
 
-day13C ::  ConduitT Integer Void (StateT Integer IO) Game
-day13C = do
+day13C :: Bool -> ConduitT Integer Void (StateT Integer IO) (Game, Score)
+day13C free = do
     icp <- readICPFromC "data/Day13-input.txt"
-    void (interpretC icp) .| play empty
+    let icp' = if free then 2:drop 1 icp else icp
+    void (interpretC icp') .| play empty 0
   where
-    play :: Map Position Tile -> ConduitT Integer Void (StateT Integer IO) Game
-    play game = do
+    play :: Game -> Score -> ConduitT Integer Void (StateT Integer IO) (Game, Score)
+    play game score = do
       maybeX   <- await
       maybeY   <- await
       maybeTID <- await
+
+      liftIO $ visualize game
       if isNothing maybeX || isNothing maybeY || isNothing maybeTID
-      then return game
+      then return (game, score)
       else do
         let (Just x)   = maybeX
         let (Just y)   = maybeY
         let (Just tid) = maybeTID
 
-        play $ insert (fromIntegral x, fromIntegral y) (case tid of
+        if x == -1 && y == 0
+        then play game tid -- tid is the new score
+        else play (update game x y tid) score
+    update game x y tid =
+      (flip.) insert (fromIntegral x, fromIntegral y) game $
+        case tid of
           0 -> Empty
           1 -> Wall
           2 -> Block
           3 -> Paddle
-          4 -> Ball) game
-
-day13CPart1 = do
-  (game,_) <- run $  yield 0 .| day13C
-  return . length . filter ((Block==).snd) $ toList game
+          4 -> Ball
 
 visualize m = renderSVG "./out/Day13-result.svg" (dims2D 500 500) (rotate halfTurn scene :: Diagram B)
   where
@@ -53,4 +58,21 @@ visualize m = renderSVG "./out/Day13-result.svg" (dims2D 500 500) (rotate halfTu
            <> listOf Ball   `atPoints` repeat (circle 0.2 # fc blue)
            <> listOf Paddle `atPoints` repeat (square 0.5 # fc white # scaleX 1.5)
 
-draw = run (yield 0 .| day13C) >>= \(g,_) -> visualize g
+day13CPart1 = do
+  ((game,_),_) <- run $ yield 0 .| day13C False
+  -- visualize g
+  return . length . filter ((Block==).snd) $ toList game
+
+day13CPart2 = do
+  ((game,score),_) <- run $ joystick .| day13C True
+--  visualize game
+  return score
+  where
+    joystick = do
+      c <- liftIO $ getChar
+      yield (case c of
+        'j' -> negate 1
+        'k' -> 0
+        'l' -> 1
+        _   -> 0)
+      joystick
--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,37 @@
 module Main where
 
-import Day1
-import Day2
-import Day3
-import Day4
-import Day5
-import Day6
-import Day7
+-- import Day1
+-- import Day2
+-- import Day3
+-- import Day4
+-- import Day5
+-- import Day6
+-- import Day7
+
+import Day13
 
 main :: IO ()
 main = do
-  -- Day1
-  day1Part1 >>= print
-  day1Part2 >>= print
-  -- Day2
-  day2Part1 >>= print
-  day2Part2 >>= print
-  -- Day3
-  day3Part1 >>= print
-  day3Part2 >>= print
-  -- Day4
-  return day4Part1 >>= print
-  return day4Part2 >>= print
-  -- Day5
-  day5
-  -- Day6
-  day6Part1 >>= print
-  day6Part2 >>= print
-  -- Day7
-  day7Part1 >>= print . maximum
+  -- -- Day1
+  -- day1Part1 >>= print
+  -- day1Part2 >>= print
+  -- -- Day2
+  -- day2Part1 >>= print
+  -- day2Part2 >>= print
+  -- -- Day3
+  -- day3Part1 >>= print
+  -- day3Part2 >>= print
+  -- -- Day4
+  -- return day4Part1 >>= print
+  -- return day4Part2 >>= print
+  -- -- Day5
+  -- day5
+  -- -- Day6
+  -- day6Part1 >>= print
+  -- day6Part2 >>= print
+  -- -- Day7
+  -- day7Part1 >>= print . maximum
+
+  -- Day13
+  day13CPart2 >>= print
+  return ()
